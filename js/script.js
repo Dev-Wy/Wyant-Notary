f9d9e5bb9a74c2ecad5030ec6d9943a39570acea
Dev Wy <123113468+Dev-Wy@users.noreply.github.com>
2025-04-25 22:10:24 -0400

    Added phone number, updated format, hero transition

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*function myFunction() {
+function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
@@ -6,24 +6,11 @@
         x.style.display = "block";
     }
 }
-    
+  
 
 const myLinks = document.getElementById("myLinks");
 
 myLinks.addEventListener("click", function()
 {
     this.classList.add("hidden");
-});*/
-
-const x = document.getElementById("myLinks");
-function myFunction() {
-  // Toggle between adding / removing the responsive class from the menu
-  x.classList.toggle("responsive");
-}
-
-// We get all the a elements with class "nav-item", and attach a click
-// listener to them which removes the responsive class from myTopNav element.
-const theLinks = document.querySelectorAll(".nav-item");
-theLinks.forEach(link => link.addEventListener("click", ()=>{
-  x.classList.remove("responsive");
-}))
+})
